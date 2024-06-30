bbdbb61ae95e9f843cd8ff734eb0bf5b5a1484f0
zhao <2076364156@qq.com>
2024-07-01 00:11:08 +0800

    上传配置文件即sys初始化文件

---
+++ SYS/sys.h
@@ -0,0 +1,12 @@
+#ifndef SYS_H
+#define SYS_H
+
+#include "config.h"
+
+void sys_init(void);
+void Delay_us(uint32_t xus);
+void Delay_ms(uint32_t xms);
+void Delay_s(uint32_t xs);
+
+#endif
+
---
+++ config.h
@@ -0,0 +1,18 @@
+#ifndef Config_h
+#define Config_h
+
+//定义使用报盒库
+#define baohe 1
+
+#include "stm32f10x.h"
+#include "stdio.h"
+#include "string.h"
+
+#if baohe
+#include "sys.h"
+//#include "led.h"
+#endif
+
+
+#endif
+
